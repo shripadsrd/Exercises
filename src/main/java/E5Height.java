d1a10817c2f62765ac1d3d4fb14359bc256967a7
Shripad Deshpande <shripad.srd@gmail.com>
2018-02-20 19:45:35 -0600

    EPI-1: Tree1

---
+++ src/main/java/E5Height.java
@@ -0,0 +1,22 @@
+public class E5Height {
+
+    public boolean isHeightBalanced(final TreeNode root) {
+        if (getHeight(root) == Integer.MIN_VALUE) return false;
+        return true;
+    }
+
+    private int getHeight(final TreeNode root) {
+        if (root == null) return 0;
+
+        final int left = getHeight(root.getLeft());
+        final int right = getHeight(root.getRight());
+        final int diff = Math.abs(left - right);
+
+        if (left == Integer.MIN_VALUE
+                || right == Integer.MIN_VALUE
+                || diff > 1) {
+            return Integer.MIN_VALUE;
+        }
+        return diff + 1;
+    }
+}
